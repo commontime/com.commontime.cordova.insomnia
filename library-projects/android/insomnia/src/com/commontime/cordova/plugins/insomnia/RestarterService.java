8e45a8fb3e8d87e22cce6aa170d1015fdce4e4ea
Graham Mackenzie <gmackenzie@commontime.com>
2017-05-25 10:17:47 +0100

    Another fix to RestarterService

--- library-projects/android/insomnia/src/com/commontime/cordova/plugins/insomnia/RestarterService.java
+++ library-projects/android/insomnia/src/com/commontime/cordova/plugins/insomnia/RestarterService.java
@@ -6,35 +6,56 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Messenger;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 public class RestarterService extends Service {
     private Messenger messenger = new Messenger(new IncomingHandler());
+    private CountDownLatch cdl;
 
     public RestarterService() {
+
     }
 
     public void onCreate() {
-        if (!MonitorSingleton.getInstance().connected) {
-            Intent i = new Intent("com.commontime.cordova.insomnia.BOOT");
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            i.setPackage(RestarterService.this.getPackageName());
-            startActivity(i);
-        }
+        // Wait and see if it connects
+        cdl = new CountDownLatch(1);
+
+        Handler h = new Handler();
+        h.post(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    cdl.await(5, TimeUnit.SECONDS);
+                } catch (InterruptedException e) {
+                }
+                if(!MonitorSingleton.getInstance().connected) {
+                    Intent i = new Intent("com.commontime.cordova.insomnia.BOOT");
+                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    i.setPackage(RestarterService.this.getPackageName());
+                    startActivity(i);
+                }
+            }
+        });
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        cdl.countDown();
         return Service.START_STICKY;
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         MonitorSingleton.getInstance().connected = true;
+        cdl.countDown();
         return messenger.getBinder();
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
         MonitorSingleton.getInstance().connected = false;
+        cdl.countDown();
         return false;
     }
 
