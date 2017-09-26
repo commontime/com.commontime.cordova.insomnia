9ea47b6d168b436092ca3cd08e8f567f8a778781
Richard Lewin <richardlewin@Richards-MacBook-Pro.local>
2017-09-26 16:53:27 +0100

    Added iOS support. Bumped version number.

--- www/insomnia.js
+++ www/insomnia.js
@@ -1,28 +1,73 @@
 
+var exec    = require('cordova/exec'),
+    channel = require('cordova/channel'),
+    _isIos = false,
+    _isAndroid = false;
+
 module.exports = {
+
+    // ANDROID
     acquireWakeLock: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, 'Insomnia', 'acquireWakeLock', []);
+        if(_isAndroid) {
+            cordova.exec(successCallback, errorCallback, 'Insomnia', 'acquireWakeLock', []);
+        }
     },
     releaseWakeLock: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, 'Insomnia', 'releaseWakeLock', []);
+        if(_isAndroid) {
+            cordova.exec(successCallback, errorCallback, 'Insomnia', 'releaseWakeLock', []);
+        }
     },
     stopBatteryOptimization: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, 'Insomnia', 'stopBatteryOptimization', []);
+        if(_isAndroid) {
+            cordova.exec(successCallback, errorCallback, 'Insomnia', 'stopBatteryOptimization', []);
+        }
     },
     isIgnoringBatteryOptimization: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, 'Insomnia', 'isIgnoringBatteryOptimization', []);
+        if(_isAndroid) {
+            cordova.exec(successCallback, errorCallback, 'Insomnia', 'isIgnoringBatteryOptimization', []);
+        }
     },
     switchOnScreenAndForeground: function (successCallback, errorCallback, options) {
-        cordova.exec(successCallback, errorCallback, 'Insomnia', 'switchOnScreenAndForeground', [options]);
+        if(_isAndroid) {
+            cordova.exec(successCallback, errorCallback, 'Insomnia', 'switchOnScreenAndForeground', [options]);
+        }
     },
     clearKeepScreenOn: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, 'Insomnia', 'clearKeepScreenOn', []);
+        if(_isAndroid) {
+            cordova.exec(successCallback, errorCallback, 'Insomnia', 'clearKeepScreenOn', []);
+        }
     },
     enableRestartService: function (successCallback, errorCallback, options) {
-        cordova.exec(successCallback, errorCallback, 'Insomnia', 'enableRestartService', [options]);
+        if(_isAndroid) {
+            cordova.exec(successCallback, errorCallback, 'Insomnia', 'enableRestartService', [options]);
+        }
     },
     enableForegroundService: function (successCallback, errorCallback, options) {
-        cordova.exec(successCallback, errorCallback, 'Insomnia', 'enableForegroundService', [options]);
+        if(_isAndroid) {
+            cordova.exec(successCallback, errorCallback, 'Insomnia', 'enableForegroundService', [options]);
+        }
+    },
+    // ANDROID
+
+    // IOS
+    enable: function (successCallback, errorCallback) {
+        if(_isIos) {
+            cordova.exec(successCallback, errorCallback, 'Insomnia', 'enable', []);
+        }
+    },
+    disable: function (successCallback, errorCallback) {
+        if(_isIos) {
+            cordova.exec(successCallback, errorCallback, 'Insomnia', 'disable', []);
+        }
     }
+    // IOS    
 
 }
+
+// Called before 'deviceready' listener will be called
+channel.onCordovaReady.subscribe(function() {
+    channel.onCordovaInfoReady.subscribe(function() {
+        _isAndroid = device.platform.match(/^android/i) !== null;
+        _isIos = device.platform.match(/^iOS/i) !== null;
+    });
+});
