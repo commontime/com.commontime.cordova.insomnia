e9b088c492b51d26985d7ce3c4ab488db632036f
Graham Mackenzie <gmackenzie@commontime.com>
2018-10-09 08:02:42 +0100

    Update AlarmBroadCastReceiver.java

--- library-projects/android/insomnia/src/com/commontime/cordova/plugins/insomnia/AlarmBroadCastReceiver.java
+++ library-projects/android/insomnia/src/com/commontime/cordova/plugins/insomnia/AlarmBroadCastReceiver.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.PowerManager;
 import android.support.v4.content.WakefulBroadcastReceiver;
+import android.util.Log;
 
 public class AlarmBroadCastReceiver extends WakefulBroadcastReceiver {
     private PowerManager.WakeLock screenWakeLock;
@@ -11,16 +12,21 @@ public class AlarmBroadCastReceiver extends WakefulBroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent)
     {
+        Log.d(TAG, "insomniatest:AlarmBroadCastReceiver:onReceive");
+        
         if (screenWakeLock == null)
         {
             PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
             screenWakeLock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP,
                     "ct:Screenlock:");
+            Log.d(TAG, "insomniatest:AlarmBroadCastReceiver:acquire");
             screenWakeLock.acquire();
         }
         Intent service = new Intent(context, BackgroundOperationsManagerService.class);
         startWakefulService(context, service);
-        if (screenWakeLock != null)
+        if (screenWakeLock != null) {
+            Log.d(TAG, "insomniatest:AlarmBroadCastReceiver:release");
             screenWakeLock.release();
+        }
     }
 }
