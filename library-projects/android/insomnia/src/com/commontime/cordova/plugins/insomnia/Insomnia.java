13acfd0d6c096cf3c1fcc9b5a8eee3e158f41a15
Graham Mackenzie <gmackenzie@commontime.com>
2018-10-16 10:30:51 +0100

    Update Insomnia.java

--- library-projects/android/insomnia/src/com/commontime/cordova/plugins/insomnia/Insomnia.java
+++ library-projects/android/insomnia/src/com/commontime/cordova/plugins/insomnia/Insomnia.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.app.NotificationChannel;
+import android.app.NotificationManager
 import android.content.ServiceConnection;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
